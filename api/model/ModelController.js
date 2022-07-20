cbf7558d12401684c019b8519037aadcd6075e55
unknown <you@example.com>
2022-07-20 08:02:18 +0700

    test

--- api/model/ModelController.js
+++ api/model/ModelController.js
@@ -1,8 +1,46 @@
-const { Aggregate } = require('mongoose');
+//const { Aggregate } = require('mongoose');
 const db = require('./model')
-
+const db2 = require('./ModelCrmsources')
 class DocController {
 
+    async display(req, res, next) {
+        try {
+            var page = req.query.page;
+            var count = req.query.count;
+            if (page) {
+                page = parseInt(page)
+                if (page < 1) { page === 1 }
+                var skip = (page - 1) * count
+                try {
+                    const data2 = await db2.find({}, { title: 1 }).skip(skip).limit(count);
+
+                    const counting2 = await db2.countDocuments({ type: '_id' });
+
+                    const finalData = data2.filter(object => object.title.includes('Độ mật'))
+                    //const finalFlash = checkData.filter(object => object.documentField.includes('Độ mật'))
+
+
+                    return res.json({
+                        counting2,
+                        //checkData,
+                        finalData
+                    })
+                } catch (e) {
+                    console.log(e)
+                }
+            } else {
+                const data2 = await db2.find({}, { title: 1 })
+                const counting2 = await db2.countDocuments({ type: '_id' });
+                return res.json({
+                    counting2,
+                    data2
+                })
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    };
+    //--------------------------------------------------------------------//
     async get(req, res, next) {
         try {
             var page = req.query.page;
@@ -25,32 +63,34 @@ class DocController {
                     //         // })
                     //     }
                     // }
-                    const finalData = data.filter(object => object.documentField.includes(''))
-                    //const newData = finalData.map(data => data.documentField).sort()
-                    function compare(a, b) {
-                        // Sử dụng toUpperCase() để chuyển các kí tự về cùng viết hoa
-                        const typeA = a.documentField.toUpperCase();
-                        const typeB = b.documentField.toUpperCase();
+                    const finalData = data.filter(object => object.documentField.includes(' '))
+                    //const newData = finalData.map(data => data.documentField)
+                    //const newNew = data.distinct(urgencyLevel, { urgencyLevel: "thng" })
+                    // function compare(a, b) {
+                    //     // Sử dụng toUpperCase() để chuyển các kí tự về cùng viết hoa
+                    //     const typeA = a.documentField.toUpperCase();
+                    //     const typeB = b.documentField.toUpperCase();
 
-                        let comparison = 0;
-                        if (typeA > typeB) {
-                            comparison = 1;
-                        } else if (typeA < typeB) {
-                            comparison = -1;
-                        }
-                        return comparison * -1;
-                    }
+                    //     let comparison = 0;
+                    //     if (typeA > typeB) {
+                    //         comparison = 1;
+                    //     } else if (typeA < typeB) {
+                    //         comparison = -1;
+                    //     }
+                    //     return comparison * -1;
+                    // }
 
-                    const final2 = finalData.sort(compare);
-                    const newData = final2.map(data => data.documentField)
+                    //const final2 = finalData.sort(compare);
+                    //const newData = final2.map(data => data.documentField)
 
 
                     return res.json({
                         counting,
-                        // data
+                        //data
                         //array
-                        //finalData
-                        newData
+                        finalData
+                        //newData
+                        //newNew
                     })
                 } catch (e) {
                     console.log(e)
@@ -90,7 +130,6 @@ class DocController {
     async delete(req, res, next) {
         const c = await db.findOneAndDelete({ _id: req.params._id });
         res.send('deleted')
-
 
     }
 }
